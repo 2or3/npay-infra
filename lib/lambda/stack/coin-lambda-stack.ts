e308ae41595db27bc0257b642d7481e35680126b
2or3 <t.nagashima.cog@gmail.com>
2021-08-28 23:14:06 +0900

    Add aws-lambda-node

--- lib/lambda/stack/coin-lambda-stack.ts
+++ lib/lambda/stack/coin-lambda-stack.ts
@@ -1,24 +1,23 @@
 import { Stack, Construct, StackProps } from '@aws-cdk/core';
-import { AssetCode, Function, Runtime } from '@aws-cdk/aws-lambda';
-import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs'
 import { CoinTableStack } from '../../dyanamo-db/coin-table-stack';
+import { Runtime } from '@aws-cdk/aws-lambda';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 
 export class CoinLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const dynamodb = new CoinTableStack(this, "CoinTableStack", {});
-    const putCoinLambda = new Function(this, "PutCoinFunction", {
-      code: new AssetCode("lib/lambda/application"),
-      handler: "put-coin.handler",
+    const dynamodb = new CoinTableStack(this, 'CoinTableStack', {});
+    const put_coin_lambda = new NodejsFunction(this, 'PutCoin', {
+      entry: 'lib/lambda/application/put-coin.ts',
       runtime: Runtime.NODEJS_14_X,
       environment: {
         TABLE_NAME: dynamodb.CoinTable.tableName,
-        PRIMARY_KEY: "UserId",
-      },
+        PRIMARY_KEY: 'UserId',
+      }
     });
 
     // dynamodb読み取り権限をLambdaに付与
-    dynamodb.CoinTable.grantReadData(putCoinLambda);
+    dynamodb.CoinTable.grantReadData(put_coin_lambda);
   }
 }
